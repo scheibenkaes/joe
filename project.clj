fc00e8c61cb537f3b2aa34fe98a964ce593ecdb5
Benjamin Klüglein <scheibenkaes@googlemail.com>
2012-01-10 22:58:04 +0100

    changed back to vimclojure

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :main joe.web
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [noir "1.2.1"]]
-  :dev-dependencies [[swank-clojure "1.3.0"] 
+  :dev-dependencies [[org.clojars.ibdknox/lein-nailgun "1.1.1"] 
                      [lein-vim "1.0.2-SNAPSHOT"]])
