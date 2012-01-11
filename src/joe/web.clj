6a80faeff8b3bcb6b1167a7ac4be8af3e902d47b
Benjamin Klüglein <scheibenkaes@googlemail.com>
2012-01-11 23:27:49 +0100

    working redirection

--- src/joe/web.clj
+++ src/joe/web.clj
@@ -5,11 +5,13 @@
             [noir.response :as response]
             [joe.storage :as storage]))
 
+(defn update-decodings! [info]
+  (let [updated (update-in info [:decoded] inc)]
+    (storage/update! updated)
+    updated))
+
 (defn respond-with-found-info [info]
-  (do 
-    (let [updated (update-in info [:decoded] inc)]
-      (storage/update! updated)
-      (response/json updated))))
+  (response/json info))
 
 (defn decode-url! [id]
   (if-let [url (storage/get-by-short-id id)]
@@ -29,6 +31,14 @@
 
 (defpage [:post "/s"] {:keys [url]}
   (save-url! url))
+
+(defn redirect-to-url [id]
+  (when-let [url (:url (storage/get-by-short-id id))]
+    (response/redirect url)))
+
+(defpage "/s/:id" {:keys [id]}
+  (-> id storage/get-by-short-id update-decodings!)
+  (redirect-to-url id))
 
 (defn -main [& args]
   (init 0)
